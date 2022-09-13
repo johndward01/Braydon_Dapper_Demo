6d59c1f6fdaba61f704b85a50d96f2cbf6b88bcc
John Ward <johndward01@gmail.com>
2022-09-13 09:03:55 -0500

    Add project files.

---
+++ Braydon_Dapper_Demo/EmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Braydon_Dapper_Demo;
+internal class EmployeeRepository
+{
+    private readonly IDbConnection _conn;
+
+	public EmployeeRepository(IDbConnection conn)
+	{
+		_conn = conn;
+	}
+
+	public IEnumerable<Employee> GetAllEmployees()
+	{
+		return _conn.Query<Employee>("SELECT * FROM Employees;");
+	}
+
+}
---
+++ Braydon_Dapper_Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Braydon_Dapper_Demo;
+using MySql.Data.MySqlClient;
+
+
+string connectionString = "Server=localhost;Database=bestbuy;Uid=root;Pwd=password;";
+
+IDbConnection conn = new MySqlConnection(connectionString);
+
+var repo = new EmployeeRepository(conn);
+
+var employees = repo.GetAllEmployees();
+
+foreach (var employee in employees)
+{
+    Console.WriteLine(employee.EmployeeID);
+    Console.WriteLine(employee.FirstName);
+    Console.WriteLine(employee.LastName);
+    Console.WriteLine();
+    Console.WriteLine();
+}
